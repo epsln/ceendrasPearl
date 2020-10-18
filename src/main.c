a9a35ab1c05dc45456cda5876116d1fdf16ae019
epsil0n <epsil0n@rose>
2020-10-18 19:26:42 +0200

    pushed array moving operation to their own file...

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 
 #include "include/plot.h"
 #include "include/complexMath.h"
+#include "include/arraysOps.h"
 
 #define SIZEARR 10000
 #define HEIGHT 1000 
@@ -35,28 +36,6 @@ void showMatrix(double complex mat[2][2]){
 
 }
 
-
-
-void matrix3dto3D(double complex matrix3d[1000][2][2], double complex destination[1000][2][2], int i, int k){
-	destination[i][0][0] = matrix3d[k][0][0];
-	destination[i][0][1] = matrix3d[k][0][1];
-	destination[i][1][0] = matrix3d[k][1][0];
-	destination[i][1][1] = matrix3d[k][1][1];
-}
-
-void matrix3dto2D(double complex matrix3d[1000][2][2], double complex destination[2][2], int i){
-	destination[0][0] = matrix3d[i][0][0];
-	destination[0][1] = matrix3d[i][0][1];
-	destination[1][0] = matrix3d[i][1][0];
-	destination[1][1] = matrix3d[i][1][1];
-}
-
-void matrix2dto3D(double complex matrix2d[2][2], double complex destination[1000][2][2], int i){
-	destination[i][0][0] = matrix2d[0][0];
-	destination[i][0][1] = matrix2d[0][1];
-	destination[i][1][0] = matrix2d[1][0];
-	destination[i][1][1] = matrix2d[1][1];
-}
 
 
 
