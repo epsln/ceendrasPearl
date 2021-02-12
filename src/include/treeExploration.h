d04a5d596fb1f4fd1141cc9bd018f8a624023102
epsil0n <martin.olivier1997@gmail.com>
2021-02-13 00:23:14 +0100

    fixed issue with turn forward declaration

--- src/include/treeExploration.h
+++ src/include/treeExploration.h
@@ -3,8 +3,8 @@
 
 void goForward(int *lev, int* tag, int* state, int FSA[19][4], double complex* word, double complex* gens);
 void goBackwards(int *lev);
-int availableTurn(int *lev, int *tag);
-void turnForward(int *lev, int *tag, double complex* word, double complex* gens);
+int availableTurn(int *lev, int *tag, int* state, int FSA[19][4]);
+void turnForward(int *lev, int *tag,int* state, int FSA[19][4], double complex* word, double complex* gens);
 
 int branchTermRepetends(int lev, int* tag, double complex fixRep[4][4], double complex* word, image_t* img);
 
