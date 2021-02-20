e9e6cf2d91acc9757de560557bbfb6995e4ff8fc
epsil0n <martin.olivier1997@gmail.com>
2021-02-20 02:29:32 +0100

    added a bailout condition on the recipes, as to not create nan including generators

--- src/recipes.c
+++ src/recipes.c
@@ -1,4 +1,6 @@
 #include <complex.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "include/recipes.h"
 
@@ -29,9 +31,14 @@ void maskitRecipe(double complex ta, double complex* gens){
 
 
 void grandmaRecipe(double complex ta, double complex tb, double complex* gens){
+	if (ta == 0 && tb == 0){
+		printf("Error ! ta and tb cannot be both == 0 !\n Exiting...\n");
+		exit(-3);
+	}
 	double complex a = 1;
 	double complex b = (-ta * tb);
-	double complex c = ta * ta + tb * tb; double complex delta = b*b - 4 * a * c; 
+	double complex c = ta * ta + tb * tb;
+       	double complex delta = b*b - 4 * a * c; 
 	double complex tab = (- b - csqrt(delta))/(2 * a); 
 	double complex z0 = ((tab - 2) * tb)/(tb * tab - 2 * ta + 2 * I * tab);
 
@@ -62,6 +69,10 @@ void grandmaSpecialRecipe(double complex ta, double complex tb, double complex t
 	double complex tc = ta * ta + tb * tb + tab * tab - ta * tb * tab - 2;
 	double complex Q  = csqrt(2 - tc);	
 	double complex R = 0;
+	if (cabs(tab) == 2){ 
+		printf("Error ! taB cannot be == +/- 2 !\n Exiting...\n");
+		exit(-3);
+	}
 	if (cabs(tc + I * Q * csqrt(tc + 2)) >= 2)
 		R = csqrt(tc + 2);
 	else
@@ -78,6 +89,16 @@ void grandmaSpecialRecipe(double complex ta, double complex tb, double complex t
 	gens[(1 * 2 + 1) * 2 + 0] = (tb * tab - 2 * ta + I * Q * tab)/(z0*(2 * tab + 4));
 	gens[(1 * 2 + 0) * 2 + 1] = ((ta * tab - 2 * ta - I * Q * tab)*z0)/(2 * tab - 4);
 	gens[(1 * 2 + 1) * 2 + 1] = (tb + 2 * I * Q)/2; 
+
+	printf("[%lf + %lf, ", creal(gens[(0 * 2 + 0) * 2 + 0]), cimag(gens[(0 * 2 + 0) * 2 + 0])); 
+	printf("%lf + %lf]\n", creal(gens[(0 * 2 + 1) * 2 + 0]), cimag(gens[(0 * 2 + 1) * 2 + 0])); 
+	printf("[%lf + %lf, ", creal(gens[(0 * 2 + 0) * 2 + 1]), cimag(gens[(0 * 2 + 0) * 2 + 1])); 
+	printf("%lf + %lf]\n", creal(gens[(0 * 2 + 1) * 2 + 1]), cimag(gens[(0 * 2 + 1) * 2 + 1])); 
+
+	printf("[%lf + %lf, ", creal(gens[(1 * 2 + 0) * 2 + 0]), cimag(gens[(1 * 2 + 0) * 2 + 0])); 
+	printf("%lf + %lf]\n", creal(gens[(1 * 2 + 1) * 2 + 0]), cimag(gens[(1 * 2 + 1) * 2 + 0])); 
+	printf("[%lf + %lf, ", creal(gens[(1 * 2 + 0) * 2 + 1]), cimag(gens[(1 * 2 + 0) * 2 + 1])); 
+	printf("%lf + %lf]\n", creal(gens[(1 * 2 + 1) * 2 + 1]), cimag(gens[(1 * 2 + 1) * 2 + 1])); 
                                 
              
 	gens[(2 * 2 + 0) * 2 + 0] =  gens[(0 * 2 + 1) * 2 + 1];
