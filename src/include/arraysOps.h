05e88b8193299233c35d82cafe0300044fa30438
epsil0n <martin.olivier1997@gmail.com>
2021-03-14 16:01:10 +0100

    made some header for souper

--- src/include/arraysOps.h
+++ src/include/arraysOps.h
@@ -1,7 +1,14 @@
 #ifndef arraysOps
 #define arraysOps
+
+#include "quatsMath.h"
+
 void matrix3dto3D(double complex* matrix3d, double complex *destination, int i, int k);
 void matrix3dto2D(double complex* matrix3d, double complex destination[2][2], int i);
 void matrix2dto3D(double complex matrix2d[2][2], double complex* destination, int i);
+
+void matrix3dto3DQuat(quat_t* matrix3d, quat_t *destination, int i, int k);
+void matrix3dto2DQuat(quat_t* matrix3d, quat_t destination[2][2], int i);
+void matrix2dto3DQuat(quat_t matrix2d[2][2], quat_t* destination, int i);
 
 #endif
--- src/include/quatsMath.h
+++ src/include/quatsMath.h
@@ -5,10 +5,15 @@ typedef struct quat_t{
 	long double a, b, c, d;
 }quat_t;
 
+
 void quatAdd(quat_t* out, quat_t a, quat_t b);
 void quatSub(quat_t* out, quat_t a, quat_t b);
 void quatMult(quat_t* out, quat_t a, quat_t b);
 void quatDiv(quat_t* out, quat_t a, quat_t b);
 
 long double quatNorm(quat_t h);
+long double quatDist(quat_t h1, quat_t h2);
+
+void matmulQuat(quat_t A[2][2], quat_t B[2][2], quat_t C[2][2]);
+void mobiusOnPointQuat(quat_t* out, quat_t T[2][2] , quat_t h);
 #endif
