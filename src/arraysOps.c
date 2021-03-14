e3797b29e7e693caa4c83f08d5208ddee1bdfba6
epsil0n <martin.olivier1997@gmail.com>
2021-03-14 15:48:32 +0100

    new array moving ops for quaternions

--- src/arraysOps.c
+++ src/arraysOps.c
@@ -1,4 +1,7 @@
 #include <complex.h>
+
+#include "include/quatsMath.h"
+
 void matrix3dto3D(double complex* matrix3d, double complex* destination, int i, int k){
 	destination[(i * 2 + 0) * 2 + 0] = matrix3d[(k * 2 + 0) * 2 + 0];
 	destination[(i * 2 + 1) * 2 + 0] = matrix3d[(k * 2 + 1) * 2 + 0];
@@ -20,4 +23,23 @@ void matrix2dto3D(double complex matrix2d[2][2], double complex* destination, in
 	destination[(i * 2 + 1) * 2 + 1] = matrix2d[1][1];
 }
 
+void matrix3dto3DQuat(quat_t* matrix3d, quat_t* destination, int i, int k){
+	destination[(i * 2 + 0) * 2 + 0] = matrix3d[(k * 2 + 0) * 2 + 0];
+	destination[(i * 2 + 1) * 2 + 0] = matrix3d[(k * 2 + 1) * 2 + 0];
+	destination[(i * 2 + 0) * 2 + 1] = matrix3d[(k * 2 + 0) * 2 + 1];
+	destination[(i * 2 + 1) * 2 + 1] = matrix3d[(k * 2 + 1) * 2 + 1];
+}
 
+void matrix3dto2DQuat(quat_t* matrix3d, quat_t destination[2][2], int i){
+	destination[0][0] = matrix3d[(i * 2 + 0) * 2 + 0];
+	destination[0][1] = matrix3d[(i * 2 + 0) * 2 + 1];
+	destination[1][0] = matrix3d[(i * 2 + 1) * 2 + 0];
+	destination[1][1] = matrix3d[(i * 2 + 1) * 2 + 1];
+}
+
+void matrix2dto3DQuat(quat_t matrix2d[2][2], quat_t* destination, int i){
+	destination[(i * 2 + 0) * 2 + 0] = matrix2d[0][0];
+	destination[(i * 2 + 0) * 2 + 1] = matrix2d[0][1];
+	destination[(i * 2 + 1) * 2 + 0] = matrix2d[1][0];
+	destination[(i * 2 + 1) * 2 + 1] = matrix2d[1][1];
+}
