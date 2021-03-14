89548e0425eab47ff6e33bd4f73bc3009d7c0d8b
epsil0n <martin.olivier1997@gmail.com>
2021-03-14 15:48:09 +0100

    added a few functions for quaternions

--- src/quatsMath.c
+++ src/quatsMath.c
@@ -35,3 +35,37 @@ void quatDiv(quat_t* out, quat_t hNum, quat_t hDenum){
 long double quatNorm(quat_t h){
 	return sqrtl(powl(h.a, 2) + powl(h.b, 2) + powl(h.c, 2) + powl(h.d, 2));
 }
+
+void matmulQuat(quat_t A[2][2], quat_t B[2][2], quat_t C[2][2]){
+	quat_t buff;
+
+	//C[0][0] = A[0][0] * B[0][0] + A[1][0] * B[0][1];
+	quatMult(&C[0][0], A[0][0], B[0][0]);
+	quatMult(&buff, A[1][0], B[0][1]);
+	quatAdd(&C[0][0], C[0][0], buff);
+
+	//C[1][0] = A[0][0] * B[1][0] + A[1][0] * B[1][1];
+	quatMult(&C[1][0], A[0][0], B[1][0]);
+	quatMult(&buff, A[1][0], B[1][1]);
+	quatAdd(&C[1][0], C[1][0], buff);
+
+	//C[0][1] = A[0][1] * B[0][0] + A[1][1] * B[0][1];
+	quatMult(&C[0][1], A[0][1], B[0][0]);
+	quatMult(&buff, A[1][1], B[0][1]);
+	quatAdd(&C[0][1], C[0][1], buff);
+
+	//C[1][1] = A[0][1] * B[1][0] + A[1][1] * B[1][1];
+	quatMult(&C[1][1], A[0][1], B[1][0]);
+	quatMult(&buff, A[1][1], B[1][1]);
+	quatAdd(&C[1][1], C[1][1], buff);
+}
+
+void mobiusOnPointQuat(quat_t* out, quat_t T[2][2] , quat_t h){
+	quat_t buff, buff1;
+	quatMult(&buff, T[0][0], h);
+	quatAdd(&buff, buff, T[1][0]);
+	quatMult(&buff1, T[0][1], h);
+	quatAdd(&buff1, buff1, T[1][1]);
+	quatDiv(&buff, buff, buff1);
+	//return (T[0][0] * z + T[1][0])/(T[0][1] * z + T[1][1]);
+}
