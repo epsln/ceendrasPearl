bd1b97ec10f1de5f2e1e001e22f9bccd262e0da8
epsil0n <martin.olivier1997@gmail.com>
2021-03-27 15:58:16 +0100

    fixed the FSA

--- src/treeExploration.c
+++ src/treeExploration.c
@@ -16,7 +16,9 @@ void goForward(int *lev, int* tag, int* state, int FSA[19][4], double complex* w
 	double complex buffGen[2][2];
 	double complex buffOut[2][2];
 
-	int idxGen = 0, i = 0;
+	int idxGen = 0, i = 1;
+	
+	//printf("goForward: %d\n", *lev);
 	
 	for (int i = 0; i < 2; i++){
 		for (int j = 0; j < 2; j++){
@@ -29,17 +31,21 @@ void goForward(int *lev, int* tag, int* state, int FSA[19][4], double complex* w
 	//printf("state[%d] = %d\n", *lev, state[*lev]);
 	//printf("FSA[%d][%d] = %d\n\n", state[*lev], modulo(tag[*lev - 1] + 1, 4),  FSA[state[*lev]][modulo(tag[*lev - 1] + 1, 4)]);
 	//printf("FSA[%d][%d] = %d\n", state[*lev + 1], modulo(tag[*lev + 1] - 1, 4), FSA[state[*lev + 1]][modulo(tag[*lev + 1] - 1, 4)]);
-	//while((FSA[state[*lev + 1]][idxGen] == -1)){
-	//	idxGen = modulo(tag[*lev + 1] - i, 4);
-	//	i--;
-	//}
+	do{
+		idxGen = modulo(tag[*lev] + i, 4);
+		//printf("FSA[%d][%d] == %d\n",state[*lev], idxGen, FSA[state[*lev]][idxGen]);
+		i--;
+	}while((FSA[state[*lev]][idxGen] == 0));
+	//printf("idxGen = %d\n", idxGen);
+
+
+	state[*lev + 1] = FSA[state[*lev]][idxGen];
+	//printf("state[%d] = %d\n", *lev + 1, state[*lev + 1]);
 
 	*lev = *lev + 1; //Advance one lvl
 
-	//state[*lev] = FSA[state[*lev]][idxGen];
-
-	tag[*lev] = modulo(tag[*lev - 1] + 1, 4); //Take the rightmost turn
-	//tag[*lev] = idxGen; //Take the rightmost turn
+	//tag[*lev] = modulo(tag[*lev - 1] + 1, 4); //Take the rightmost turn
+	tag[*lev] = idxGen; //Take the rightmost turn
 
 
 	matrix3dto2D(word, buffWord, *lev - 1);
@@ -50,36 +56,49 @@ void goForward(int *lev, int* tag, int* state, int FSA[19][4], double complex* w
 }
 
 void goBackwards(int *lev){
+	//printf("goBack\n");
 	*lev = *lev - 1;
 }
 
 int availableTurn(int *lev, int* tag, int* state, int FSA[19][4]){
+//	printf("availableTurn\n");
+//	printf("FSA[%d][%d] = %d\n\n", state[*lev], modulo(tag[*lev + 1] - 1, 4), FSA[state[*lev]][modulo(tag[*lev + 1] - 1, 4)]);
 	if (*lev == -1)	return 0;
 
-
-	if (modulo(tag[*lev + 1] - 1,  4) == modulo(tag[*lev] + 2 , 4)){
+	if((FSA[state[*lev]][modulo(tag[*lev + 1] - 1, 4)] == 0))
 		return 0;
-	}
-	else{
+
+	//if (modulo(tag[*lev + 1] - 1,  4) == modulo(tag[*lev] + 2 , 4))
+	//	return 0;
+	
+	else
 		return 1;
-	}
+	
 }	
 
 void turnForward(int *lev, int *tag, int* state, int FSA[19][4], double complex* word, double complex* gens){
 	double complex buffWord[2][2];
 	double complex buffGen[2][2];
 	double complex buffOut[2][2];
-	int idxGen = 0, i = 0;
+	int idxGen = 0, i = 1;
+	//printf("turnForward\n");
 
-	tag[*lev + 1] = modulo(tag[*lev + 1] - 1,  4);
+	//tag[*lev + 1] = modulo(tag[*lev + 1] - 1,  4);
 	
 
-	//while((FSA[state[*lev + 1]][idxGen] == -1)){
-	//	idxGen = modulo(tag[*lev + 1] - i, 4);
-	//	i--;
-	//}
-	//state[*lev + 1] = idxGen;
-	state[*lev + 1] = modulo(tag[*lev + 1] - 1,  4) ;
+	do{	
+		idxGen = modulo(tag[*lev + 1] - i, 4);
+	//	printf("FSA[%d][%d] == %d\n",state[*lev], idxGen + 1, FSA[state[*lev]][idxGen]);
+		i--;
+	}while((FSA[state[*lev]][idxGen] == 0));
+
+//	printf("idxGen = %d\n", idxGen);
+	state[*lev + 1] = FSA[state[*lev]][idxGen];
+//	printf("state[%d] = %d\n", *lev + 1, state[*lev + 1]);
+
+	tag[*lev + 1] = idxGen; 
+//	printf("tag[%d] = %d\n\n", *lev + 1, tag[*lev + 1]);
+	//state[*lev + 1] = modulo(tag[*lev + 1] - 1,  4) ;
 
 	if (*lev == -1)
 		matrix3dto3D(gens, word, 0, tag[0]);
@@ -111,6 +130,10 @@ int branchTermRepetends(int lev, int* tag, double complex fixRep[4][4], double c
 
 	//If we hit the maximum length of word (-1 because of 0 indexing, bailout !)
 	//Used only in case of point mode
+	//printf("lvl: %d\n", lev);
+	//printf("%lf %lf\n",creal(z0), cimag(z0));
+	//printf("%lf %lf\n",creal(z1), cimag(z1));
+	//printf("%lf %lf\n\n",creal(z2), cimag(z2));
 	if (img->line == 1 && lev == img->maxword - 1){
 		return 1;
 	}
@@ -141,7 +164,10 @@ int branchTermRepetends(int lev, int* tag, double complex fixRep[4][4], double c
 		//y2 = (int) map(cimag(z2), -2 * img->bounds, 0, img->h, 0);
 		//x3 = (int) map(creal(z3), -aspectRatio * img->bounds, aspectRatio * img->bounds, 0, img->w);
 		//y3 = (int) map(cimag(z3), 0, 2 * img->bounds, img->h, 0);
+		//
 		//printf("%lf %lf\n",creal(z0), cimag(z0));
+		//printf("%lf %lf\n",creal(z1), cimag(z1));
+		//printf("%lf %lf\n",creal(z2), cimag(z2));
 		line(x0, y0, x1, y1, img);	
 		line(x1, y1, x2, y2, img);	
 		point(x0, y0, img);
@@ -179,32 +205,41 @@ void computeDepthFirst(double complex* gens, image_t* img, int numIm){
 
 	memset(state, 0, img->maxword * sizeof(int));
 	state[0] = 1;//We start at a
+	tag[0] = 0;
 
 	//State automaton array:
 	//Given the current state and the next generator, gives the next state
 	//If possible, try to find an algo that generates this sort of stuff
+	
 	int FSA[19][4] = {
-		{ 1,  2,  3,  4},//Identity
-		{ 1,  5, -1,  4},//a
-		{ 7,  2,  8, -1},//b
-		{-1,  2,  8,  6},//A
-		{ 9, -1, 10,  4},//B
-		{ 7,  2, 11, -1},//ab
-		{12, -1, 10,  4},//AB
-		{ 1,  5, -1, 13},//ba
-		{-1,  2,  3, 13},//bA
-		{ 1,  5, -1,  4},//Ba
-		{-1, 16,  3,  4},//BA
-		{-1,  2,  3, 17},//abA
-		{ 1, 18, -1,  4},//ABa
-		{ 9, -1, -1,  4},//baB
-		{-1, -1, 10,  4},//bAB
-		{ 7,  2, -1, -1},//Bab
-		{-1,  2,  8, -1},//BAb
-		{-1, -1, 10,  4},//abAB
-		{ 7,  2, -1, -1} //ABab
+		{ 1,  2,  3,  4},//Identity 0 
+		{ 1,  5,  0,  4},//a        1
+		{ 7,  2,  8,  0},//b        2
+		{ 0,  2,  3,  6},//A        3
+		{ 9,  0, 10,  4},//B        4
+		{ 7,  2, 11,  0},//ab       5
+		{12,  0, 10,  4},//AB       6
+		{ 1,  5,  0, 13},//ba       7
+		{ 0,  2,  3, 14},//bA       8
+		{ 1, 15,  0,  4},//Ba       9 
+		{ 0, 16,  3,  4},//BA       10
+		{ 0,  2,  3, 17},//abA      11
+		{ 1, 18,  0,  4},//ABa      12
+		{ 9,  0,  0,  4},//baB      13
+		{ 0,  0, 10,  4},//bAB      14
+		{ 7,  2,  0,  0},//Bab      15
+		{ 0,  2,  8,  0},//BAb      16
+		{ 0,  0, 10,  4},//abAB     17
+		{ 7,  2,  0,  0} //ABab     18
 	};
-
+		
+	//int FSA[5][4] = {
+	//	{ 1,  2,  3,  4},//Identity 0 
+	//	{ 1,  2,  0,  4},//a        1
+	//	{ 1,  2,  3,  0},//b        2
+	//	{ 0,  2,  3,  4},//A        3
+	//	{ 1,  0,  3,  4}//B        4
+	//};
 	int *plev;
 	plev = &lev;
 
@@ -225,6 +260,7 @@ void computeDepthFirst(double complex* gens, image_t* img, int numIm){
 
 		//Might need to put another exit condition check right here...
 		if (lev == -1 && tag[0] == 1) break;	
+
 		turnForward(plev, tag, state, FSA, word, gens);
 		printWord(lev, tag, img);
 	}
